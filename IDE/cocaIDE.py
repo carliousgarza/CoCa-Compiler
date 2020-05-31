2f5eafabb9905948aef3321eba9392338eb343fc
Carlos <carlosfgarza@gmail.com>
2020-05-31 15:54:08 -0500

    saved files now run in IDE

--- IDE/cocaIDE.py
+++ IDE/cocaIDE.py
@@ -2,16 +2,17 @@ from tkinter import *
 import sys
 import os
 
-def run_clicked():
+def save_clicked():
     # Get code from the codeArea
     code = codeArea.get('0.0', 'end-1c')
-    print(code)
     # Get and write to file
     fileName = 'textEditorFile.coca'
     file = open(fileName, 'w')
     file.write(code)
     file.close
-    print("File saved, now running...")
+    console.insert('1.0', 'Saved File\n')
+
+def run_clicked():
     os.system('python3 ../main.py textEditorFile.coca')
 
 # Root Window
@@ -31,8 +32,13 @@ console.configure(background='black',foreground="#000fff000")
 #console.configure(background='black',foreground="#000fff000", state="disabled")
 console.pack()
 
-# Run button
-runButton = Button(root, text="Save & Run", command=run_clicked)
+# Save button
+runButton = Button(root, text="Save", command=save_clicked)
 runButton.pack()
+
+# Run button
+runButton = Button(root, text="Run", command=run_clicked)
+runButton.pack()
+
 
 root.mainloop()
